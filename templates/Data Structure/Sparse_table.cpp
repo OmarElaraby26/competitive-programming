0075945b2ea67d04a2dd6dd649795951edd69f83
Omar Elaraby <47684373+OmarElaraby26@users.noreply.github.com>
2021-09-02 18:11:42 +0200

    Update Sparse_table.cpp

--- templates/Data Structure/Sparse_table.cpp
+++ templates/Data Structure/Sparse_table.cpp
@@ -1,58 +1,71 @@
 template<typename T>
-T combine(const T &a, const T &b) { // combine here = max(a, b)
-	return a > b ? a : b;
+// sparse_table min
+T sp_min(const T &a, const T &b) { // combine here = max(a, b)
+    return a < b ? a : b;
 }
 
-template<typename T, T (*combine)(const T&, const T&)>
+template<typename T>
+// sparse_table max
+T sp_max(const T &a, const T &b) { // combine here = max(a, b)
+    return a > b ? a : b;
+}
+
+template<typename T>
+// sparse_table gcd
+T sp_gcd(const T &a, const T &b) { // combine here = max(a, b)
+    return __gcd(a, b);
+}
+
+template<typename T, T (*combine)(const T &, const T &)>
 class Sparse_table {
 private:
-	vector<vector<T> > table;
+    vector<vector<T> > table;
 public:
-	Sparse_table() {
-	}
-	template<typename TT>
-	Sparse_table(const TT &v) { // TT = vector<T> or deque<T>
-		build_sparse_table(v);
-	}
+    Sparse_table() {
+    }
 
-	template<typename TT>
-	void build_sparse_table(const TT &v) { // TT = vector<T> or deque<T>
-		const int n = v.size();
+    template<typename TT>
+    Sparse_table(const TT &v) { // TT = vector<T> or deque<T>
+        if(v.size()) build(v);
+    }
 
-		table.assign(__lg(n) + 1, vector<T>(n));
+    template<typename TT>
+    void build(const TT &v) { // TT = vector<T> or deque<T>
+        const int n = v.size();
+        table.assign(__lg(n) + 1, vector<T>(n));
 
-		int idx = 0; // for 0 based array
-		for (const T &val : v) {
-			table[0][idx++] = val;
-		}
+        int idx = 0; // for 0 based array
+        for (const T &val: v) {
+            table[0][idx++] = val;
+        }
 
-		for (int row = 1; row < (int) table.size(); ++row) {
-			const int c_range = (1 << row);
-			for (int col = 0; col + c_range - 1 < (int) table[row].size();
-					++col) {
-				table[row][col] = combine(table[row - 1][col],
-						table[row - 1][col + (c_range >> 1)]);
-			}
-		}
-	}
+        for (int row = 1; row < (int) table.size(); ++row) {
+            const int c_range = (1 << row);
+            for (int col = 0; col + c_range - 1 < (int) table[row].size();
+                 ++col) {
+                table[row][col] = combine(table[row - 1][col],
+                                          table[row - 1][col + (c_range >> 1)]);
+            }
+        }
+    }
 
-	T query_o1(int l, int r) const { // overlap friendly
-		if (l > r) swap(l, r);
-		int msb = __lg(r - l + 1), s_range = (1 << msb);
-		return combine(table[msb][l], table[msb][r - s_range + 1]);
-	}
+    T query_o1(int l, int r) const { // overlap friendly
+        if (l > r) swap(l, r); // or to use assert ???
+        int msb = __lg(r - l + 1), s_range = (1 << msb);
+        return combine(table[msb][l], table[msb][r - s_range + 1]);
+    }
 
-	T query_ologn(int l, int r) const { // NON overlap friendly functions as +, * ..
-		if (l > r) swap(l, r);
-		T ans;
-		bool started = 0; // not to add initall value to ans
-		while (l <= r) {
-			int msb = __lg(r - l + 1);
-			T nxt = table[msb][l];
-			l += (1 << msb);
-			if (started) ans = combine(ans, nxt);
-			else ans = nxt, started = true;
-		}
-		return ans;
-	}
+    T query_ologn(int l, int r) const { // NON overlap friendly functions as +, * ..
+        if (l > r) swap(l, r); // or to use assert ???
+        T ans;
+        bool started = 0; // not to add initall value to ans
+        while (l <= r) {
+            int msb = __lg(r - l + 1);
+            T nxt = table[msb][l];
+            l += (1 << msb);
+            if (started) ans = combine(ans, nxt);
+            else ans = nxt, started = true;
+        }
+        return ans;
+    }
 };
